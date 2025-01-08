50bda3e17bd379d97538886ce804f4b571f3bf0c
vorges-data <vinicius.jborges36@gmail.com>
2025-01-08 00:07:54 -0300

    Aula 01 concluída - Comandos básicos

---
+++ 01_git_python/exemplo_00.py
@@ -0,0 +1,13 @@
+print(input("Digite um número: "))
+
+idade = input("Digite sua idade: ")
+
+# Variáveis
+numero = 10 # int
+nome_usuario = "Vinicius" # str
+numero_decimal = 10.5 # float
+verdadeiro = True # bool
+falso = False # bool
+print(type(numero))
+
+
